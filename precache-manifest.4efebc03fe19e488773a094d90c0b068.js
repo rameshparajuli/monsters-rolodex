441d69c8d6a81f4e20590784239cbb0bc23f8d1f
rmsparajuli <rameshparajuli09@gmail.com>
2020-03-22 10:27:17 +0545

    Updates

---
+++ precache-manifest.4efebc03fe19e488773a094d90c0b068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41516ed36803eb25fb8454e5561763c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d09281843cb0f941620b",
+    "url": "/monsters-rolodex/static/css/main.2cf15648.chunk.css"
+  },
+  {
+    "revision": "b57d2044e2511261ff03",
+    "url": "/monsters-rolodex/static/js/2.b530339c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b530339c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09281843cb0f941620b",
+    "url": "/monsters-rolodex/static/js/main.de36137d.chunk.js"
+  },
+  {
+    "revision": "694e23953260e5598f1d",
+    "url": "/monsters-rolodex/static/js/runtime-main.3043fc22.js"
+  }
+]);
